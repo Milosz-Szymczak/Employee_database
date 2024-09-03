a646cf07d3beb41e5ac4c2b45461f7538f4d26e4
Miłosz Szymczak <milosz.szymczak@protonmail.com>
2024-09-03 14:48:19 +0200

    Updated authorization configuration

--- src/main/java/pl/milosz/employees_database/security/SecurityConfig.java
+++ src/main/java/pl/milosz/employees_database/security/SecurityConfig.java
@@ -1,25 +1,38 @@
 package pl.milosz.employees_database.security;
 
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http
-                .authorizeHttpRequests(authz -> authz.anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+                        .requestMatchers(HttpMethod.DELETE, "/api/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/**").permitAll()
+                        .requestMatchers(HttpMethod.PATCH, "/api/**").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/api/**").permitAll()
+                        .anyRequest().authenticated())
                 .oauth2ResourceServer((oauth) -> oauth
-                .jwt(Customizer.withDefaults()))
+                        .jwt(Customizer.withDefaults()))
                 .build();
     }
 }
